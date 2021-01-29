ee39c54361f7cf5d91d3731cf1b11b6599f9951f
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-01-29 21:46:13 +0530

    feat(middleware): add redirection middleware, fixes #1

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { AuthenticationParams } from './interface/authenticate.interface';
 import { Config } from './interface/config.interface';
 
 class UniAuth {
   private configs: Array<Config> = [];
 
-  constructor(config: Array<Config>) {}
+  constructor(configs: Array<Config>) {
+    this.configs = configs;
+    this.configs.forEach((config) => this.fillDefaults(config));
+  }
 
+  /**
+   * Method to fill default values and ensure that params are passed right
+   * @param config Config parameter passed by user
+   */
   private fillDefaults(config: Config) {
+    if (!config.name || !config.url) {
+      throw new Error('name and url required');
+    }
+
     if (!config.endpoint) {
       config.endpoint = {
         auth: 'account/o/login',
@@ -13,6 +27,33 @@ class UniAuth {
       };
     }
   }
-}
 
-const a = new UniAuth([{ name: 'uniauth', url: 'http://localhost:5000' }]);
+  /**
+   * Returns the config by searching the passed values by name
+   * @param string name :: name of config object to use
+   */
+  private getConfigByName(name: string): Config {
+    const item = this.configs.filter((item) => item.name === name);
+    if (item.length === 0) {
+      throw new Error(`Config named ${name} was not found`);
+    } else if (item.length > 1) {
+      throw new Error(`Configs with duplicate name :: ${name} found`);
+    } else {
+      return item[0];
+    }
+  }
+
+  /**
+   * Express Middleware
+   */
+  public authenticate(name: string, params?: AuthenticationParams) {
+    const config = this.getConfigByName(name);
+
+    /** return an express middleware */
+    return function (req: Request, res: Response, next: NextFunction) {
+      const loginUrl = `${config.url}/${config.endpoint.auth}?client_id=${config.clientId}&redirect_uri=${config.redirectUri}`;
+      res.redirect(loginUrl);
+      next();
+    };
+  }
+}
--- src/interface/config.interface.ts
+++ src/interface/config.interface.ts
@@ -6,6 +6,11 @@ export interface Config {
 
   url: string;
 
+  /** application configs */
+  clientId: string;
+  clientSecret: string;
+  redirectUri: string;
+
   /** option method to set custom endpoints if using modified server */
   endpoint?: {
     auth: string;
